feb69656b34d3f9a3f9c3ad5363ba4ad33c4b2d4
Faedar <awwalpole@gmail.com>
2019-04-02 21:15:38 +1000

    Compass Testing
    Started Initial testing on compass updating so that we can fix compass update bugs when more than 2 players have joined a lobby

--- 4PlayerCoop/MPScriptLoad.cs
+++ 4PlayerCoop/MPScriptLoad.cs
@@ -15,65 +15,9 @@ namespace MPLimitRemover
             //On.StoreManager.OnGameLobbyJoinRequested += new On.StoreManager.hook_OnGameLobbyJoinRequested(GameLobbyJoinRequest);
             On.ConnectPhotonMaster.CreateOrJoin += new On.ConnectPhotonMaster.hook_CreateOrJoin(CreateJoinRoom);
             On.ConnectPhotonMaster.CreateRoom_1 += new On.ConnectPhotonMaster.hook_CreateRoom_1(CreateStoreRoom);
+            On.OtherPlayersCompass.Update += new On.OtherPlayersCompass.hook_Update(CompassUpdate);
             //Put in patched methods here
         }
-
-        /*public void GameLobbyJoinRequest(On.StoreManager.orig_OnGameLobbyJoinRequested original, StoreManager self, string _roomName)
-        {
-            string text = string.Empty;
-            if (!DemoManager.DemoIsActive)
-            {
-                if (Global.GamePaused)
-                {
-                    PauseMenu.Pause(false);
-                }
-                if (NetworkLevelLoader.Instance.IsOverallLoadingDone && NetworkLevelLoader.Instance.ContinueAfterLoading && SplitScreenManager.Instance.LocalPlayerCount == 1)
-                {
-                    Character firstLocalCharacter = CharacterManager.Instance.GetFirstLocalCharacter();
-                    if (firstLocalCharacter == null || firstLocalCharacter.IsDead)
-                    {
-                        text = "MessageBox_Network_CannotJoinAtThisMoment";
-                    }
-                    else if (true)
-                    {
-                        self.JoinRequestFromLobbyID(_roomName);
-                    }
-                    else
-                    {
-                        self.SetPendingRequestConnectionToRoom(_roomName);
-                    }
-                }
-                else if (Global.Lobby.PlayersInLobby.Count == 0)
-                {
-                    text = "MessageBox_Network_JoinNoCharacter";
-                }
-                else if (Global.Lobby.LocalPlayerCount == 2 || SplitScreenManager.Instance.LocalPlayerCount > 1)
-                {
-                    text = "MessageBox_Network_JoinSplitscreen";
-                }
-                else
-                {
-                    text = "Connection_Error_JoinRoomFailed";
-                }
-            }
-            else
-            {
-                text = "MessageBox_Network_JoinTutorial";
-            }
-            if (!string.IsNullOrEmpty(text))
-            {
-                string loc = LocalizationManager.Instance.GetLoc(text);
-                CharacterUI characterUI = SplitScreenManager.Instance.GetCharacterUI(0);
-                if (characterUI)
-                {
-                    characterUI.MessagePanel.Show(loc, -1f, null);
-                    if (characterUI.PauseMenu && characterUI.PauseMenu.IsDisplayed)
-                    {
-                        characterUI.PauseMenu.Hide();
-                    }
-                }
-            }
-        }*/
 
         public void CreateJoinRoom(On.ConnectPhotonMaster.orig_CreateOrJoin original, ConnectPhotonMaster self, string _roomName)
         {
@@ -105,6 +49,15 @@ namespace MPLimitRemover
             roomOptions.isVisible = true;
             roomOptions.maxPlayers = PlayerLimitRemover.PlayerLimit;
             PhotonNetwork.CreateRoom(_roomName, roomOptions, TypedLobby.Default);
+        }
+
+        private float timer = 5f;
+        public void CompassUpdate(On.OtherPlayersCompass.orig_Update original, OtherPlayersCompass self)
+        {
+            original.Invoke(self);
+            timer -= Time.deltaTime;
+            if (timer <= 0f)
+                Debug.Log(Global.Lobby.PlayersInLobby.Count);
         }
     }
 }
